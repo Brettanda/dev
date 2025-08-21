3afaffecdf72a3189ffb89fc6acaba5eee530fe6
Brettanda <brett@brettanda.ca>
2025-08-20 23:39:49 -0600

    auto commit

--- .local/scripts/fix-drivers-post-enabling-secure-boot.sh
+++ .local/scripts/fix-drivers-post-enabling-secure-boot.sh
@@ -4,4 +4,10 @@
 # This should be run right after enabling secure boot and you find that your graphics
 # drivers aren't loading
 
-sudo update-secureboot-policy --enroll-key
+DISTRO="$(lsb_release -i | cut -f 2-)"
+
+if [ $DISTRO == "Linuxmint"]; then
+  sudo update-secureboot-policy --enroll-key
+else
+  echo "Found this for linux mint use so it must only work on that distro :) maybe" 
+fi
